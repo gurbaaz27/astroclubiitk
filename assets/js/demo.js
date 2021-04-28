bfb4f5f4bef2f48d887921d9e049e020132fcb85
gurbaaz27 <gurbaazsn123@gmail.com>
2021-04-28 22:48:02 +0530

    save theme fix

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,7 +1,28 @@
 $(function() {
-    $("body").toggleClass("page-dark-mode");
+    var theme;
+
+    if (localStorage.getItem('theme')) {  
+      theme = localStorage.getItem('theme');  
+    } else {  
+      theme = 'dark';  
+    }
+    console.log(theme);
+    if (theme == 'dark') {
+      $("body").toggleClass("page-dark-mode");
+    }
+
+    localStorage.setItem('theme', theme);
+
     $('#change-skin').on('click', function () {
       $("body").toggleClass("page-dark-mode");
       BeautifulJekyllJS.initNavbar();
+
+      if (localStorage.getItem('theme') == 'dark') {
+        localStorage.setItem('theme', 'light'); 
+        console.log("dark removed") ; 
+      } else {
+        localStorage.setItem('theme', 'dark');
+        console.log("dark added") ;
+      }
     });
   });
